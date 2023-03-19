9c981f35e1394bece5e525b350e85f46f4cf281c
Tindell Lockett <tindell@lockett.us>
2023-03-19 13:18:03 -0400

    Add interactive user approval for suggested commit message.

--- commitmeant.py
+++ commitmeant.py
@@ -3,60 +3,58 @@ import subprocess
 import openai
 
 def main():
-    # Read the API key from the api_key fileimport os
-
     api_key_path = os.path.expanduser('~/projects/ScriptPack/openai.key')
     with open(api_key_path, "r") as api_key_file:
         OPENAI_API_KEY = api_key_file.read().strip()
 
-    # Set the API key for the openai library
     openai.api_key = OPENAI_API_KEY
 
-    # Ensure the working directory is a git repository
     try:
         subprocess.run(["git", "rev-parse", "--is-inside-work-tree"], check=True, capture_output=True)
     except subprocess.CalledProcessError:
         print("This script must be run inside a git repository.")
         exit(1)
 
-    # Stage all current changes
     subprocess.run(["git", "add", "."])
 
-    # Check for uncommitted changes
     if not subprocess.run(["git", "diff-index", "--quiet", "HEAD", "--"]).returncode:
         print("No changes detected. Exiting.")
         exit(0)
 
-    # Get the git diff
     git_diff = subprocess.run(["git", "diff",  "--cached", "--no-color"], capture_output=True, text=True).stdout
 
     print("Raw diff: ", git_diff)
 
-    # Generate the commit message using the OpenAI Chat API
-    messages = [
-        {"role": "system", "content": "Generate a short and concise commit message based on the following git diff:"},
-        {"role": "user", "content": git_diff},
-    ]
+    commit_message = ""
+    user_accepts = False
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages,
-        max_tokens=100,
-        n=1,
-        temperature=0.5,
-    )
+    while not user_accepts:
+        messages = [
+            {"role": "system", "content": "Generate a short and concise commit message based on the following git diff:"},
+            {"role": "user", "content": git_diff},
+        ]
 
-    # Print the raw API response
-    print("Raw API response:", response)
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            max_tokens=100,
+            n=1,
+            temperature=0.5,
+        )
 
-    commit_message = response.choices[0].message["content"].strip()
+        print("Raw API response:", response)
 
-    # Commit the changes with the generated message
+        commit_message = response.choices[0].message["content"].strip()
+
+        print(f"Suggested commit message: {commit_message}")
+        user_input = input("Is this commit message acceptable? (yes/no): ").lower()
+
+        if user_input in ["yes", "y"]:
+            user_accepts = True
+
     subprocess.run(["git", "commit", "-m", commit_message])
 
     print(f"Committed changes with message: {commit_message}")
-
-
 
 if __name__ == "__main__":
     main()
