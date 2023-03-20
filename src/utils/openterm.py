860c0c7e784581aa989a40e87e34e457e0019743
Tindell Lockett <tindell@lockett.us>
2023-03-20 17:51:48 -0400

    some readme and comment changes

--- src/utils/openterm.py
+++ src/utils/openterm.py
@@ -4,7 +4,7 @@ import sys
 def main(name):
     if not name:
         sys.exit(0)
-    # Start a new terminal window and run the clipboard_transformer.py script with the selected prompt
+    # Start a new terminal window and run the provided script.  It doesn't work with args yet.
     if sys.platform == "darwin":  # macOS
         os.system(f"osascript -e 'tell application \"Terminal\" to do script \""+name+"; exit\" activate'")
     else:
