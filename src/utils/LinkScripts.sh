f2519e9c291f5ab858059665ce9a75f3e7138833
Tindell Lockett <tindell@lockett.us>
2023-03-20 23:05:00 -0400

    Add script to link CLI scripts to /usr/local/bin and execute them with pipenv in current directory.

---
+++ src/utils/LinkScripts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set the source directory and the target directory
+
+script_path=$(readlink -f "$0")
+script_directory=$(dirname $(dirname $(dirname "$script_path")))
+src_directory="$script_directory/src/cli"
+target_directory="/usr/local/bin"
+symlink_target="$script_directory/src/utils/runInCD"
+
+
+# Loop through all .py files in the src_directory
+for file in "$src_directory"/*.py; do
+    # Get the filename without the .py extension
+    filename=$(basename "$file" .py)
+    
+    # Remove the existing symlink in the target_directory, if it exists
+    sudo rm -i "$target_directory/$filename"
+    
+    # Create the new symlink in the target_directory
+    sudo ln -s "$symlink_target" "$target_directory/$filename"
+done
+
